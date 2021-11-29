a1ccd721b3a0211cbdf48664cf105efbb20ae558
JastiDeepu <jastideepu@gmail.com>
2021-11-29 18:46:12 +0530

    final commit

---
+++ src/datatype/Intsamples.java
@@ -0,0 +1,13 @@
+package datatype;
+
+public class Intsamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=1;
+		int j=2;
+		System.out.println(i+j);
+	}
+
+}
